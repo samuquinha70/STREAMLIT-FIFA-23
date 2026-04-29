2ea1059e9f485600bf7f0af85ea150716874cef6
Cristiano Kemmer <criskemmer@gmail.com>
2026-04-29 12:09:38 -0500

    chore: Formatting code

--- pages/players.py
+++ pages/players.py
@@ -1,11 +1,14 @@
 import base64
-import streamlit as st
 import requests
+import streamlit as st
+
+
 @st.cache_data
 def load_image_64(url):
     headers = {"User-Agent": "Mozilla/5.0"}
     data = requests.get(url, headers=headers).content
     return "data:image/png;base64," + base64.b64encode(data).decode()
+
 
 st.set_page_config(
     page_title='Players',
@@ -14,9 +17,7 @@ st.set_page_config(
 
 )
 
-
-df_data =  st.session_state['data']
-
+df_data = st.session_state['data']
 
 clubes = df_data['Club'].value_counts().index
 club = st.sidebar.selectbox('Clube', clubes)
@@ -35,14 +36,14 @@ st.markdown(f'**Clube:** {player_stats["Position"]}')
 
 col1, col2, col3, col4 = st.columns(4)
 col1.markdown(f'**Idade:**{player_stats['Age']}')
-col2.markdown(f'**Altura:**{player_stats['Height(cm.)']/ 100}')
-col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f}")
+col2.markdown(f'**Altura:**{player_stats['Height(cm.)'] / 100}')
+col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)'] * 0.453:.2f}")
 st.divider()
 
 st.subheader(f'Overall{player_stats['Overall']}')
 st.progress(int(player_stats['Overall']))
 
 col1, col2, col3, col4 = st.columns(4)
-col1.metric(label='Valor de mercado', value=f'£ {player_stats['Value(£)']:,}' )
-col2.metric(label='Remuneração semanal', value=f'£ {player_stats['Wage(£)']:,}' )
-col3.metric(label='Cláusula de rescisão', value=f'£ {player_stats['Release Clause(£)']:,}' )
+col1.metric(label='Valor de mercado', value=f'£ {player_stats['Value(£)']:,}')
+col2.metric(label='Remuneração semanal', value=f'£ {player_stats['Wage(£)']:,}')
+col3.metric(label='Cláusula de rescisão', value=f'£ {player_stats['Release Clause(£)']:,}')
