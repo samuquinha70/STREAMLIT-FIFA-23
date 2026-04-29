8366e1d47540f1daa98030973917269057c5589c
Cristiano Kemmer <criskemmer@gmail.com>
2026-04-29 12:03:52 -0500

    chore: Do not exceed 120 chars with markdown text

--- home.py
+++ home.py
@@ -11,20 +11,22 @@ if 'data' not in st.session_state:
     st.session_state['data'] = df_data
 
 
-st.markdown('# Fifa23 Official Dataset! ⚽')
+st.markdown('# FIFA 23 Official Dataset! ⚽')
 st.sidebar.markdown('Desenvolvido por Samuel')
 
 btn = st.button('Acesse os dados no Kaggle')
 if btn: 
     webbrowser.open_new_tab('https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data')
+
 st.markdown(
-   
-'O conjunto de dados de jogadores de futebol de 2017 a 2023 fornece informações abrangentes sobre jogadores de futebol profissionais. O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos dos jogadores, características físicas, estatísticas de jogo, detalhes de contratos e afiliações a clubes.'
+'O conjunto de dados de jogadores de futebol de 2017 a 2023 fornece informações abrangentes sobre jogadores de futebol '
+'profissionais. O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos dos jogadores, '
+'características físicas, estatísticas de jogo, detalhes de contratos e afiliações a clubes.'
+)
 
-
-'**Com mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para analistas, pesquisadores e entusiastas do futebol interessados ​​em explorar vários aspectos do mundo do futebol, pois permite o estudo de atributos de jogadores, métricas de desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e desenvolvimento de jogadores ao longo do tempo.'
-
-
-
-
+st.markdown(
+'**Com mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para analistas, pesquisadores e '
+'entusiastas do futebol interessados em explorar vários aspectos do mundo do futebol, pois permite o estudo de '
+'atributos de jogadores, métricas de desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores '
+'e desenvolvimento de jogadores ao longo do tempo.'
 )
